1ce70ccbf00659dbc1e8e2751ecc39f520309ce1
Patricia Naana Agyare <81566295+Patricianaana@users.noreply.github.com>
2024-02-21 13:09:53 +0000

    edited create page

--- resources/views/saveData.blade.php
+++ resources/views/saveData.blade.php
@@ -11,24 +11,38 @@
     <h1>Saving Employees Data In The Database</h1>
     <form action="users" method="POST">
         @csrf
-        <input type="text" name="name" placeholder="Enter full name here">
-        <br>
-        <span style="color: red">@error('name'){{ $message }}@enderror</span>
-        {{-- the span tag is to validate the user's input --}}
-        <br>
-        <input type="text" name="email" placeholder="Enter email here">
-        <br>
-        <span style="color: red">@error ('email'){{ $message }}@enderror</span>
-        <br>
-        <input type="text" name="address" placeholder="Enter address here">
-        <br>
-        <span style="color: red">@error('address'){{ $message }}@enderror</span>
-        <br>
-        <input type="text" name="contact" placeholder="Enter contact here">
-        <br>
-        <span style="color: red">@error('contact'){{ $message }}@enderror</span>
-        <br>
-        <button type="submit">Add Employee</button>
+        <div class="" style="margin-bottom: 10px">
+            <input type="text" name="name" placeholder="Enter your full name"><br>
+            @error('name')
+                <span style="color: red">
+                    {{ $message }}
+                </span>
+            @enderror
+        </div>
+        <div class="" style="margin-bottom: 10px">
+            <input type="text" name="email" placeholder="Enter your email"><br>
+            @error('email')
+                <span style="color: red">
+                    {{ $message }}
+                </span>
+            @enderror
+        </div>    
+        <div class="" style="margin-bottom: 10px">
+            <input type="text" name="address" placeholder="Enter your address"><br>
+            @error('address')
+                <span style="color: red">
+                    {{ $message }}
+                </span>
+            @enderror
+        </div>
+        <div class="" style="margin-bottom: 10px">
+            <input type="text" name="contact" placeholder="Enter your contact here"><br>
+            @error('contact')
+                <span style="color: red">
+                    {{ $message }}
+                </span>
+            @enderror
+        </div> <button type="submit">Add Employee</button>
     </form>
 </body>
 </html>
